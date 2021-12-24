9713d2b0cce62c5e98deb1aae1b6c8dc2c241968
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-12-24 01:04:57 -0500

    [ ADD ] Previous action [ ADD ] Logic to display Get Pokemones, Anterior and Siguiente button dinamically

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -4,18 +4,30 @@ import React from 'react'
 //useDispatch: allows us to consume the action
 //useSelector: allows us to read the array of dataInicial
 import { useDispatch, useSelector } from 'react-redux'
-import { obtenerPokemonesAccion, siguientePokemonAccion } from '../redux/pokeDucks'
+import { obtenerPokemonesAccion, siguientePokemonAccion, anteriorPokemonAccion } from '../redux/pokeDucks'
 
 const Pokemones = () => {
     const dispatch = useDispatch()
 
     const pokemones = useSelector(store => store.pokemones.results)
+    const next = useSelector(store => store.pokemones.next)
+    const previous = useSelector(store => store.pokemones.previous)
     console.log(pokemones)
     return (
         <div>
             Lista de Pokemones
-            <button onClick={()=> dispatch(obtenerPokemonesAccion())}>Get Pokemones</button>
-            <button onClick={()=> dispatch(siguientePokemonAccion())}>Siguiente</button>
+            {
+                pokemones.length === 0 &&
+                <button onClick={()=> dispatch(obtenerPokemonesAccion())}>Get Pokemones</button>
+            }
+            {
+                previous &&
+                <button onClick={()=> dispatch(anteriorPokemonAccion())}>Anterior</button>
+            }
+            {   
+                next &&
+                <button onClick={()=> dispatch(siguientePokemonAccion())}>Siguiente</button>
+            }
             <ul>
                 {
                     pokemones.map(pokemon => <li key={pokemon.name}>{pokemon.name}</li>)
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -10,6 +10,7 @@ const dataInicial = {
 // types
 const OBTENER_POKEMONES_EXITO = 'OBTENER_POKEMONES_EXITO'
 const SIGUIENTE_POKEMONES_EXITO = 'SIGUIENTE_POKEMONES_EXITO'
+const ANTERIOR_POKEMONES_EXITO = 'ANTERIOR_POKEMONES_EXITO'
 
 // reducer
 export default function pokeReducer(state =dataInicial, action){
@@ -19,6 +20,8 @@ export default function pokeReducer(state =dataInicial, action){
         // With ...action.payload we're getting out the properties of the object
             return {...state, ...action.payload}
         case SIGUIENTE_POKEMONES_EXITO:
+            return {...state, ...action.payload}
+        case ANTERIOR_POKEMONES_EXITO:
             return {...state, ...action.payload}
         default:
             return state
@@ -53,6 +56,22 @@ export const siguientePokemonAccion = () => async (dispatch, getState) => {
         const res = await axios.get(next)
         dispatch({
             type: SIGUIENTE_POKEMONES_EXITO,
+            payload: res.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const anteriorPokemonAccion = () => async (dispatch, getState) => {
+    // const offset = getState().pokemones.offset
+    // const siguiente = offset + 20
+    const { previous } = getState().pokemones
+    try {
+        const res = await axios.get(previous)
+        dispatch({
+            type: ANTERIOR_POKEMONES_EXITO,
             payload: res.data
         })
     } catch (error) {
