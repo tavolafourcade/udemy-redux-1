2b976468585f02676bb4ccf088d316bfed53cb94
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-02 19:36:23 -0500

    [ ADD ] Disabling acceder button while the user is using the Auth API from Gmail

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { ingresoUsuarioAccion } from '../redux/usuarioDucks'
 
 const Login = () => {
     const dispatch = useDispatch()
+    const loading = useSelector(store => store.usuario.loading)
     return (
         <div className='mt-5 text-center'>
             <h3>Ingreso con Google</h3>
             <hr />
             <button 
                 className='btn btn-dark' 
-                onClick={()=> dispatch(ingresoUsuarioAccion())}>Acceder</button>
+                onClick={()=> dispatch(ingresoUsuarioAccion())}
+                disabled={loading}>
+                    Acceder
+            </button>
         </div>
     )
 }
