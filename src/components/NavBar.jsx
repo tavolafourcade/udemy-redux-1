71fca9c2b8f2231f30a2cd6c1376e7053eac8c40
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-02 21:35:42 -0500

    [ UPDATE ] Show certain NavBar elements depending if the user is logged

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { cerrarSesionAccion } from '../redux/usuarioDucks'
 import {withRouter} from 'react-router-dom'
 
@@ -10,17 +10,28 @@ const NavBar = (props) => {
         dispatch(cerrarSesionAccion())
         props.history.push('/login')
     }
+    const activo = useSelector(store => store.usuario.activo)
+
     return (
         <div className='navbar navbar-dark bg-dark'>
             <Link className='navbar-brand mx-auto' to='/'>APP Poke</Link>
             <div className="d-flex">
-                <NavLink className='btn btn-dark mr-2' to='/' exact>Inicio</NavLink> 
-                <NavLink className='btn btn-dark mr-2' to='/login'>Login</NavLink> 
-                <button 
-                    className='btn btn-dark mr-2'
-                    onClick={() => cerrarSesion()}>
-                        Cerrar Sesión
-                </button>
+                {
+                    activo ? (
+                    <>
+                        <NavLink className='btn btn-dark mr-2' to='/' exact>Inicio</NavLink> 
+                        <button 
+                            className='btn btn-dark mr-2'
+                            onClick={() => cerrarSesion()}>
+                                Cerrar Sesión
+                        </button>
+                    </>
+                    ) : (
+                        <NavLink className='btn btn-dark mr-2' to='/login'>Login</NavLink> 
+                    ) 
+                        
+                }
+                
 
             </div>
         </div>
