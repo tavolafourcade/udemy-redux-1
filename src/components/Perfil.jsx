edeaa1bacfdaabc2714898a78eadb7258233a0c0
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-06 15:07:57 -0500

    [ ADD ] Show user info in Perfil component

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Perfil = () => {
+    const usuario = useSelector(store => store.usuario.user)
+    console.log('Usuario using useSelector', usuario)
     return (
-        <div>
-            Perfil
+        <div className='mt-5 text-center'>
+            <div className='card'>
+                <div className="card-body">
+                    <img src={usuario.photoURL} alt="" className='img-fluid'/>
+                    <h5 className='card-title'>Nombre: {usuario.displayName}</h5>
+                    <p className='card-text'>Email: {usuario.email}</p>
+                    <button className='btn btn-dark'>
+                        Editar nombre
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
