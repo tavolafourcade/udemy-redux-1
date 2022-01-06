c027b08c406fb6402888a94ffd80cc92364fbc42
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-06 17:13:10 -0500

    [ ADD ] Login input to the Perfil component to edit Users name

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from 'react-redux'
 
 const Perfil = () => {
     const usuario = useSelector(store => store.usuario.user)
+    //We're going to save into the state what the user writes in the input field
+    const [nombreUsuario, setNombreUsuario] = useState(usuario.displayName)
+    const [activarFormulario, setActivarFormulario] = useState(false)
+
     console.log('Usuario using useSelector', usuario)
+
+    const actualizarUsuario = () => {
+
+    }
     return (
         <div className='mt-5 text-center'>
             <div className='card'>
@@ -11,10 +19,37 @@ const Perfil = () => {
                     <img src={usuario.photoURL} alt="" className='img-fluid'/>
                     <h5 className='card-title'>Nombre: {usuario.displayName}</h5>
                     <p className='card-text'>Email: {usuario.email}</p>
-                    <button className='btn btn-dark'>
+                    <button className='btn btn-dark' onClick={()=>setActivarFormulario(true)}>
                         Editar nombre
                     </button>
                 </div>
+                {
+                    activarFormulario && (
+                        <div className="card-body">
+                            <div className="row justify-content-center">
+                                <div className="col-md-5">
+                                    <div className="input-group mb-3">
+                                        <input 
+                                            type="text" 
+                                            className="form-control"
+                                            value={nombreUsuario} 
+                                            onChange={e => setNombreUsuario(e.target.value)}                                    
+                                        />
+                                        <div className="input-group-append">
+                                            <button 
+                                                className="btn btn-dark" 
+                                                type="button" 
+                                                onClick={()=> actualizarUsuario()}
+                                            >
+                                                Actualizar
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
